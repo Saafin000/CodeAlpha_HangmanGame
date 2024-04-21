54a131b2ebba09f750d4e0ca732001ccd1a93e56
Saafin000 <147503825+Saafin000@users.noreply.github.com>
2024-04-21 22:09:23 +0530

    Add files via upload

---
+++ HangmanGame.py
@@ -0,0 +1,29 @@
+import random
+
+f = open("Hangman_words.txt", "r")
+data = f.readlines()
+words = [word.strip().upper() for word in data]  
+
+word = random.choice(words)
+total_chances = 7
+guessed_word = "-" * len(word)
+
+while total_chances != 0:
+    print("Current word:", guessed_word)
+    letter = input("Guess a letter: ").upper()
+    if letter in word:
+        for index in range(len(word)):
+            if word[index] == letter:
+                guessed_word = guessed_word[:index] + letter + guessed_word[index + 1:]
+        if guessed_word == word:
+            print("Congratulations you won !!!")
+            break
+    else:
+        total_chances -= 1
+        print("Incorrect guess. \nChances left:", total_chances)
+
+if total_chances == 0:
+    print("Game Over")
+    print("You Lose")
+    print("All the chances are exhausted")
+    print("The correct word is:", word)
